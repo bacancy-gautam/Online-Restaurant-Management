62e83ed59bc80c721e0763a11f75c6ee7b5a0736
jinal-Bacancy <jinal.vandara@bacancytechnology.com>
2018-03-19 16:06:05 +0530

    static-pages controller views added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+
   resources :categories
+
+  get 'static_pages/home'
+
+  get 'static_pages/about'
+
+  get 'static_pages/contact'
+
+  root 'static_pages#home'
+
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
