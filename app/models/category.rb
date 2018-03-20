1d538428b1ef4680dba4624b873ed5bdfe99ee77
swati-bacancy <swati.chauhan@bacancytechnology.com>
2018-03-20 12:37:41 +0530

    added categories CRUD

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,5 @@
 class Category < ApplicationRecord
-	 has_and_belongs_to_many :restaurants
+	  has_and_belongs_to_many :restaurants
+    validates :name, :presence => true,uniqueness: { scope: :name,
+    message: "category allready added" }
 end
