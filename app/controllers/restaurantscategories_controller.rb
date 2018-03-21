c386eb6b57d8203bfde47b7b8b7da0007ce4e246
swati-bacancy <swati.chauhan@bacancytechnology.com>
2018-03-21 12:37:09 +0530

    change the code according review and add gem rubocop

--- app/controllers/restaurantscategories_controller.rb
+++ app/controllers/restaurantscategories_controller.rb
@@ -1,5 +1,6 @@
 class RestaurantscategoriesController < ApplicationController
   before_action :authenticate_user!
+  
   def new
   end
   def create
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,4 @@
 class Category < ApplicationRecord
-	  has_and_belongs_to_many :restaurants
-    validates :name, :presence => true,uniqueness: { scope: :name ,
-    message: "category allready added" }
-
+	has_and_belongs_to_many :restaurants
+  validates :name, :presence => true,uniqueness: { scope: :name ,message: "category allready added" }
 end
