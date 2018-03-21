033bec0298dfece46107d100d1d5e3c3bca08048
jinal-Bacancy <jinal.vandara@bacancytechnology.com>
2018-03-21 10:18:41 +0530

    removing devise:user in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+  resources :food_items
+  resources :categories
+  resources :restaurants
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'static_pages/home'
