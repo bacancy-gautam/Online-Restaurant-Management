89dc09b4c264d6b4addf6d1ec4bb7dd57811ba57
pinkal-bacancy <pinkal.darji@bacancytechnology.com>
2018-03-21 19:07:43 +0530

    CRUD of master order

--- app/models/master_order.rb
+++ app/models/master_order.rb
@@ -1,3 +1,7 @@
 class MasterOrder < ApplicationRecord
 	has_many :orders
+  enum order_type: ["homedelivery","pickup"]
+  enum payment_type: ["cod","card"]
+  enum order_status: ["placed","inprogress","ready","deliverd"]
+  enum payment_status: ["pending","paid"]
 end
