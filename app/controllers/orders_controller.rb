eae9a6cab69266f13294509d334380cf5bab4a7c
pinkal-bacancy <pinkal.darji@bacancytechnology.com>
2018-03-21 19:07:43 +0530

    CRUD of master order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,4 @@
 class OrdersController < ApplicationController
-	before_action :authenticate_user!
 	 #before_action :current_order
   def new
     @order=Order.new
@@ -18,13 +17,16 @@ class OrdersController < ApplicationController
    		session[:order]={}
  		end
   	@order= Order.create(order_params)
+    @order.update_attribute(:price,@order.food_item.price.to_i)
+   
   	# puts "------------------#{@order.errors.full_messages}"
-  	session[:order][@order.id]= {order_id:@order.id,quantity:@order.quantity}
+  	session[:order][@order.id]= {order_id:@order.id,quantity:@order.quantity,price:@order.price.to_i}
   	#session[:order] || = @order
     redirect_to orders_path
   end
   def index
     @orders=Order.all
+  
   end
   def edit
   end
@@ -32,10 +34,9 @@ class OrdersController < ApplicationController
     @order=Order.find(params[:id])
   end
   def destroy
-    @order=Order.find(params[:id])
+    @order=Order.find(params[:id])  
+    session[:order][@order.id.to_s]=nil
     @order.destroy
-    session[:order]["@order.id"]
-    exit  
     redirect_to orders_path
   end
   private
