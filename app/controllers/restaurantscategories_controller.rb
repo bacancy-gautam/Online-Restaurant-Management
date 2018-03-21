5f7b60738706805ea3864c3ad752527f000fdd49
swati-bacancy <swati.chauhan@bacancytechnology.com>
2018-03-21 11:38:44 +0530

    #12 creating a restaurant category controller add category edit category

--- app/controllers/restaurantscategories_controller.rb
+++ app/controllers/restaurantscategories_controller.rb
@@ -17,13 +17,31 @@ class RestaurantscategoriesController < ApplicationController
     end
     redirect_to restaurantscategories_path
   end
-
   def show
+    @restaurant=Restaurant.find(params[:id])
+    @user=current_user
+    @category=Hash.new
+    id=@restaurant.id
+      @category[id]=@restaurant.categories
+    exit
   end
-
   def edit
+    @restaurant=Restaurant.find(params[:id])
   end
-
+  def update
+    @restaurant=Restaurant.find(params[:id])
+    @restaurant.categories.delete_all
+    @category = params[:restaurant][:category_ids]
+    @category = @category.reject { |c| c.empty? }
+    @category_array=[]
+    @category.each do |category|
+      @category_array.push(Category.find(category))
+    end
+    @category_array.each do |category|
+      category.restaurants<<@restaurant
+    end
+    redirect_to restaurantscategories_path
+  end
   def index
     @user=current_user
     @category=Hash.new
@@ -31,6 +49,5 @@ class RestaurantscategoriesController < ApplicationController
     @restaurant.each do |restaurant|
       @category[restaurant.id]=restaurant.categories
     end
-  
   end  
 end
