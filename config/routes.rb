8ddb09687da65a08ca317049de4fa89038124a3b
vishal-bacancy <vishal.jariwala@bacancytechnology.com>
2018-03-23 10:50:13 +0530

    white space improvement

--- config/routes.rb
+++ config/routes.rb
@@ -3,29 +3,21 @@ Rails.application.routes.draw do
   get "restaurants/new_release" => 'restaurants#new_release', :as => :new_release
   resources :orders
   resources :master_orders
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'offers/change_category'
   resources :food_items
   resources :categories
   resources :restaurants
   resources :restaurantscategories
-
   resources :users, :path => 'customers' do
     collection do
       get :change_password_edit
       patch :change_password_update
     end
   end
-
-
-
   get :search, controller: :restaurants
   get :location, controller: :restaurants
-  
-
   resources :offers
-
   get 'static_pages/home'
   get 'static_pages/index'
   get 'static_pages/about'
@@ -33,20 +25,16 @@ Rails.application.routes.draw do
   get 'users/edit'
   root 'static_pages#home'
   #root 'restaurants#new'
-
-  #root to: 'restaurants#new'
-  
+  #root to: 'restaurants#new'  
   resources :delivery_boys, :path => 'deliveryboys' do
     collection do
       get :change_password_edit
       patch :change_password_update
     end
   end
-
   devise_for :users,
   					 :controllers => { :registrations => "users/registrations",
   					 :omniauth_callbacks => 'users/omniauth_callbacks'}
-
   devise_for :delivery_boys, 
               controllers: 
               {
