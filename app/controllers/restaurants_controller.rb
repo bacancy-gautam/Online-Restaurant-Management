a22557179fb2fba5736e5a029345f5f8b77ebf2b
harshil-bacancy <harshil.patel@bacancytechnology.com>
2018-04-02 15:29:05 +0530

    Ratyrate gem added.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -2,6 +2,7 @@
 class RestaurantsController < ApplicationController
   before_action :fetch_restaurant, only: [:show, :edit,
                                           :update, :add_restaurant_to_fav]
+  # before_action :authenticate_user!
 
   def new
     @restaurant = Restaurant.new
