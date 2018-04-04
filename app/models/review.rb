4e7169ca8bd68cd4b8d0daa07e30d0c5b32d2b28
viren-bacancy <viren.dhanwani@bacancytechnology.com>
2018-04-04 13:53:18 +0530

    #67 stars dont require reload,changes in display of ratings

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,16 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
   default_scope -> { order(created_at: :desc) }
+
+  def user_reviews
+    Review.where(user_id: self.user_id).count()
+  end
+
+  def user_rating
+   rating =  Rate.where(rater_id: self.user_id, rateable_id: self.restaurant_id).pluck(:stars)
+   if rating != []
+    sum = rating.inject :+
+    avg = sum/rating.length
+   end
+  end
 end
