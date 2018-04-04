ef53bc73dc24e3d06589ff572c977a386c44ff5f
poonam-Bacancy <poonam.shah@bacancytechnology.com>
2018-04-04 17:43:22 +0530

    #56 add default status true in user and restaurant

--- db/migrate/20180328071647_add_status_to_restaurants.rb
+++ db/migrate/20180328071647_add_status_to_restaurants.rb
@@ -1,6 +1,6 @@
 # migration
 class AddStatusToRestaurants < ActiveRecord::Migration[5.1]
   def change
-    add_column :restaurants, :status, :boolean
+    add_column :restaurants, :status, :boolean , default: true
   end
 end
