51687c5004dc19154cc824af44c88de251de3386
jinal-Bacancy <jinal.vandara@bacancytechnology.com>
2018-04-09 16:17:05 +0530

    #56 change in identiation

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,6 @@
 # Controller for Static Pages
 class StaticPagesController < ApplicationController
   layout 'admin_panel', except: [:home, :about, :index, :contact]
-  before_action :force_json, only: :search_in_admin 
   def home
     # @restaurant = Restaurant.find(params[:id])
     @restaurant = Restaurant.all.includes(:address).sample(3)
@@ -14,10 +13,5 @@ class StaticPagesController < ApplicationController
   def contact; end
 
   def my_account; end
-
-  private 
-
-    def force_json 
-      request.format = :json 
-    end 
+ 
 end
